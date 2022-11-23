794c84b6d250e3e2c61d7870381de16370ce6ea6
Vitor Barbosa <vitorricardosilvabarbosa@gmail.com>
2022-11-22 22:32:46 -0300

    Fix project settings

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   projectId: 'zew9s7',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      baseUrl: 'https://wlsf82-hacker-stories.web.app';
-    }
+    setupNodeEvents(on, config) { },
+    baseUrl: 'https://wlsf82-hacker-stories.web.app'
   }
 });
