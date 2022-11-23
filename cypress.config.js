70013cff628145ca956916bb45afa51799450dff
Vitor Barbosa <vitorricardosilvabarbosa@gmail.com>
2022-11-22 21:24:49 -0300

    Project configurations

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'zew9s7',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       baseUrl: 'https://wlsf82-hacker-stories.web.app';
-    },
-  },
+    }
+  }
 });
