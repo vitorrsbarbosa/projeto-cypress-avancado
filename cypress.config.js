87a0d9c4641a702e9a4ba50cb3d2e096475a79ba
Vitor Barbosa <vitorricardosilvabarbosa@gmail.com>
2022-11-23 09:06:20 -0300

    lint:fix

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: 'zew9s7',
   e2e: {
-    setupNodeEvents(on, config) { },
+    setupNodeEvents (on, config) { },
     baseUrl: 'https://wlsf82-hacker-stories.web.app'
   }
-});
+})
